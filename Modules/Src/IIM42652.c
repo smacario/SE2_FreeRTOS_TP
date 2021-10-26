7d0ddc9df208ce39b55616ed0f5433535fd73f70
Santiago Macario <smacario@outlook.com>
2021-10-26 14:28:17 -0300

    Added init function

--- Modules/Src/IIM42652.c
+++ Modules/Src/IIM42652.c
@@ -31,3 +31,48 @@ HAL_StatusTypeDef IIM42652_WriteRegister( IIM42652 *dev, uint8_t reg, uint8_t *d
 {
 	return HAL_I2C_Mem_Write( dev->i2cHandle, IIM42652_I2C_ADDRESS, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
 }
+
+
+uint8_t IIM42652_Init( IIM42652 *dev, I2C_HandleTypeDef *i2cHandle )
+{
+
+	/* Initialise structure parameters */
+	dev->i2cHandle = i2cHandle;
+
+	dev->acc[0] = 0.0f;
+	dev->acc[1] = 0.0f;
+	dev->acc[2] = 0.0f;
+
+	dev->gyr[0] = 0.0f;
+	dev->gyr[1] = 0.0f;
+	dev->gyr[2] = 0.0f;
+
+	dev->temp_c = 0.0f;
+
+	/* I2C transaction errors */
+	uint8_t errNum = 0;
+	HAL_StatusTypeDef status;
+
+	/* Check for device ID */
+	uint8_t regDataID;
+
+	status = IIM42652_ReadRegister(dev, WHO_AM_I_ADD, &regDataID);
+	errNum += ( status != HAL_OK );
+
+	/* If ID not equal to IIM42652_I2C_ID, Init returns with an error code */
+	if( regDataID != IIM42652_I2C_ID ) return 0xFF;
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
